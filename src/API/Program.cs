3a389fef9f7d1c1cf3ad422d40010c00f5f3b8d5
ShreyashHeshi <shreyash.heshi@gmail.com>
2026-06-14 16:08:01 +0530

    Add Serilog structured logging

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -10,8 +10,18 @@ using System.Text;
 using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi;
 using Asp.Versioning;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(
+        "Logs/log-.txt",
+        rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
